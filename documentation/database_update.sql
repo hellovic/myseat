d5595dbc0fa44023d77a430a8a353826d8bb07a5
sm00f <borttenburger@sm00fs-Mac.local>
2011-03-12 08:00:13 +0100

    dokumentation changes

--- documentation/database_update.sql
+++ documentation/database_update.sql
@@ -1,10 +1,10 @@
 
-/* Run the following commands in your phpmyAdmin SQL console */
+/* ATTENTION: Run the following commands in your phpmyAdmin SQL console */
+
 
 /* Update the database from v3 to XT */
 /* --------------------------------- */
 
-/* Add a year field to outlets */
 ALTER TABLE  `outlets` ADD  `saison_year` INT( 4 ) NOT NULL AFTER  `saison_end`;
 ALTER TABLE  `outlets` ADD  `passerby_max_pax` INT( 12 ) NOT NULL;
 ALTER TABLE  `outlets` CHANGE  `avg_duration`  `avg_duration` VARCHAR( 5 ) NOT NULL;
@@ -14,10 +14,35 @@ INSERT INTO `l16n` (`id`, `needle`, `en`, `de`, `fr`, `es`, `nl`, `dk`, `se`, `i
 (NULL, '_already_user_1', 'The unsername', 'Den Benutzername', 'The unsername', 'The unsername', 'The unsername', 'The unsername', 'The unsername', 'The unsername', 'The unsername', 'The unsername', 'The unsername', 'The unsername'), 
 (NULL, '_already_user_2', 'does already exist.', 'gibt es bereits.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.', 'does already exist.');
 
+
 /* Update the database from XT 0.1730 to > XT 0.1731 */
 /* ------------------------------------------------- */
 
 ALTER TABLE `properties` ADD `logo_filename` VARCHAR( 255 ) NOT NULL ,
 ADD `status` VARCHAR( 10 ) NOT NULL DEFAULT 'active';
+
 ALTER TABLE `settings` ADD `contactform_color_scheme` VARCHAR( 12 ) NOT NULL DEFAULT 'grey',
 ADD `contactform_background` VARCHAR( 7 ) NOT NULL DEFAULT 'E0ECDB';
+
+
+/* Update the database from XT 0.1732 to > XT 0.174 */
+/* ------------------------------------------------- */
+
+ALTER TABLE `reservations` ADD `reservation_advertise` VARCHAR( 5 ) NOT NULL DEFAULT 'NO';
+ALTER TABLE `plc_users` ADD `confirm_code` VARCHAR( 255 ) NOT NULL;
+ALTER TABLE `outlets` ADD `1_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `1_close_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `2_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `2_close_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `3_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `3_close_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `4_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `4_close_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `5_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `5_close_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `6_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `6_close_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `0_open_time` time DEFAULT NULL;
+ALTER TABLE `outlets` ADD `0_close_time` time DEFAULT NULL;
+
+
