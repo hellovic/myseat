bdf7a915b64b8ce6e4f2060a415e381ff3f16b5c
sm00f <borttenburger@sm00fs-Mac.local>
2011-04-26 22:38:37 +0200

    widget

--- web/includes/outlets_detail.inc.php
+++ web/includes/outlets_detail.inc.php
@@ -49,13 +49,13 @@
 		echo "<br/><span class='bold'>"._property." :</span><br/>";
 		echo "<input type='text' name='' class='width-450' value='".$global_basedir."contactform/index.php?prp=".$row->property_id."'/>";
 		//echo "<br/><span class='bold'>Reclame Box :</span><br/>";
-		/* echo "<textarea class='script-box'>".stripslashes("
+		echo "<textarea class='script-box'>".stripslashes("
 <script type='text/javascript'>
 	var propertyID = '".$row->property_id."';
 	var outletID = '".$row->outlet_id."';
 </script>
 <script src='".$global_basedir."widget/reclamebox.php'></script>").
-		"</textarea>"; */
+		"</textarea>"; 
 		?>
 	</p>	 	 	 	 	 	 	
 </div></div></div> <!-- end left column -->
